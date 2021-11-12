d30bf4820e5ddf5bec0ac9a88ed0755384706e29
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-12 12:41:12 +0530

    Update mongo.py

--- MashaRoBot/mongo.py
+++ MashaRoBot/mongo.py
@@ -2,7 +2,7 @@ import asyncio
 import sys
 
 from motor import motor_asyncio
-from MashaRobot import MONGO_DB_URI 
+from MashaRoBot import MONGO_DB_URI 
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from MashaRobot.conf import get_int_key, get_str_key
