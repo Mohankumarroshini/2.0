e8c8e3325d73e114db626dee32de57994b7baf9b
おい aaman <heyaaman2004@gmail.com>
2021-11-12 13:43:57 +0530

    Update shippering.py

--- MashaRoBot/modules/shippering.py
+++ MashaRoBot/modules/shippering.py
@@ -77,6 +77,6 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
 
 
 __help__ = """
- ❍ /couples - To Choose Couple Of The Day
+ `/couples` - To Choose Couple Of The Day
  """
 __mod_name__ = "sʜɪᴘᴘᴇʀɪɴɢ😍"
