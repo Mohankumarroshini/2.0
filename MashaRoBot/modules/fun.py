5ebd2c2f498fdd7f2974db779ea79a749f80a6e5
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-14 12:34:05 +0530

    Update fun.py

--- MashaRoBot/modules/fun.py
+++ MashaRoBot/modules/fun.py
@@ -2,18 +2,18 @@ import html
 import random
 import time
 
-import LaylaRobot.modules.fun_strings as fun_strings
-from LaylaRobot import dispatcher
-from LaylaRobot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
-from LaylaRobot.modules.helper_funcs.chat_status import is_user_admin
-from LaylaRobot.modules.helper_funcs.alternate import typing_action
-from LaylaRobot.modules.helper_funcs.filters import CustomFilters
-from LaylaRobot.modules.helper_funcs.extraction import extract_user
+import MashaRoBot.modules.fun_strings as fun_strings
+from MashaRoBot import dispatcher
+from MashaRoBot.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
+from MashaRoBot.modules.helper_funcs.chat_status import is_user_admin
+from MashaRoBot.modules.helper_funcs.alternate import typing_action
+from MashaRoBot.modules.helper_funcs.filters import CustomFilters
+from MashaRoBot.modules.helper_funcs.extraction import extract_user
 from telegram import ChatPermissions, ParseMode, Update
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, run_async, CommandHandler, Filters
 
-import LaylaRobot.modules.helper_funcs.string_store as fun
+import MashaRoBot.modules.helper_funcs.string_store as fun
 
 GIF_ID = "CgACAgQAAx0CSVUvGgAC7KpfWxMrgGyQs-GUUJgt-TSO8cOIDgACaAgAAlZD0VHT3Zynpr5nGxsE"
 
