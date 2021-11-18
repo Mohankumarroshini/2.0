747be2c538fd1a7c2a8cdc366dee8335a33ee5cd
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-18 12:45:30 +0530

    Update users.py

--- MashaRoBot/modules/users.py
+++ MashaRoBot/modules/users.py
@@ -11,10 +11,10 @@ from telegram.ext import (
     run_async,
 )
 
-import KazukoBot.modules.sql.users_sql as sql
-from KazukoBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from KazukoBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from KazukoBot.modules.sql.users_sql import get_all_users
+import MashaRoBot.modules.sql.users_sql as sql
+from MashaRoBot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from MashaRoBot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from MashaRoBot.modules.sql.users_sql import get_all_users
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
