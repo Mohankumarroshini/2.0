76c54d6d96ec6d964649d05dab3e84e449a09bb4
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2021-11-29 14:41:42 +0530

    Update imgeditor.py

--- MashaRoBot/modules/imgeditor.py
+++ MashaRoBot/modules/imgeditor.py
@@ -450,7 +450,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             await removebg_sticker(client, query.message)
 
 
-__mod_name__ = "💞Image Editor💞"
+__mod_name__ = "ɪᴍɢ ᴇᴅɪᴛᴏʀ💞"
 __help__ = """
 <b> Image Editor </b>
 Sophiazy have some advanced image editing tools inbuilt
