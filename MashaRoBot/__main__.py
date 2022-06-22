08aae3abfec70f58fd2804cb751f533c8d5cdc6b
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2022-06-22 08:37:46 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -111,7 +111,7 @@ WOLFX_IMG = (
       "https://telegra.ph/file/89e069ddc5c581a3501ef.jpg",
      )
 
-TEXXT = ( "Heya :) PM me if you have any questions on how to use me!", , "*Hey* [{}](tg://settings/), *Athenaprobot Came for you! Since:* `{}` )
+TEXXT = ("Heya :) PM me if you have any questions on how to use me!", , "*Hey* [{}](tg://settings/), *Athenaprobot Came for you! Since:* `{}`")
 
 DONATE_STRING = """No need.. I'm rich"""
 
