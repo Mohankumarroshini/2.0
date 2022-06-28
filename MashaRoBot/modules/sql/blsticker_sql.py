fec1107abe902a748bfcdc3e3faf444305bd516a
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2022-06-28 15:27:30 +0530

    Update blsticker_sql.py

--- MashaRoBot/modules/sql/blsticker_sql.py
+++ MashaRoBot/modules/sql/blsticker_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from VegetaRobot.modules.sql import BASE, SESSION
+from MashaRoBot.modules.sql import BASE, SESSION
 from sqlalchemy import Column, BigInteger, String, UnicodeText, distinct, func
 
 
